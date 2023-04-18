b1e72665db589bc981417a0298401834c960c2b5
Harsh Rastogi <rastogiharsh04@gmail.com>
2023-04-18 16:18:11 +0530

    create navbar - create navbar component - add variable for screen sizes

--- ui/web-portal/src/app/shared/shared.module.ts
+++ ui/web-portal/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ButtonComponent } from './button/button.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-	declarations: [ButtonComponent],
+	declarations: [ButtonComponent, NavBarComponent],
 	imports: [CommonModule],
-	exports: [ButtonComponent],
+	exports: [ButtonComponent, NavBarComponent],
 })
 export class SharedModule {}
