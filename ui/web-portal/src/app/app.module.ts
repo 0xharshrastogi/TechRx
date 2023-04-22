e84e63ba6caa744c8d9606d95134b4b47b52b5c8
Harsh Rastogi <rastogiharsh04@gmail.com>
2023-04-22 15:08:51 +0530

    create sign up page

--- ui/web-portal/src/app/app.module.ts
+++ ui/web-portal/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './page/login-page/login-page.component';
+import { SignupPageComponent } from './page/signup-page/signup-page.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-	declarations: [AppComponent, LoginPageComponent],
+	declarations: [AppComponent, LoginPageComponent, SignupPageComponent],
 	imports: [BrowserModule, AppRoutingModule, SharedModule],
 	providers: [],
 	bootstrap: [AppComponent],
