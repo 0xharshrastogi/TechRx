9beadd7f9f48d8b78185284c9b3cf8c68cda5efa
Manpreet Kaur <manpreet.lawane@gmail.com>
2023-05-05 16:40:33 +0530

    Language column added in Database

--- cmd/TechRxProject/TechRxApp/models.py
+++ cmd/TechRxProject/TechRxApp/models.py
@@ -3,15 +3,17 @@ from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
-class Languages:
-	id = models.PositiveIntegerField()
-	language = models.CharField(max_length=255)
-
+# class Languages:
+# 	id = models.PositiveIntegerField()
+# 	language = models.CharField(max_length=255)
+#
 
 class Users(AbstractUser):
 	name = models.CharField(max_length=255)
 	gender = models.PositiveIntegerField()
-	languages = models.CharField(validators=[Languages], max_length=255, blank=True)
+	LANGUAGE_CHOICES = [("Eng", "English"), ("Hin", "Hindi"), ("Pun","Punjabi")]
+	# [(English, "English"),(Hindi, "Hindi"),(French, "French"),(Japanese, "Japanese")]
+	LANGUAGES = models.CharField(max_length=50, choices=LANGUAGE_CHOICES,default="English")
 	address = models.JSONField()
 	email = models.CharField(max_length=255, unique=True)
 	password = models.CharField(max_length=255)
