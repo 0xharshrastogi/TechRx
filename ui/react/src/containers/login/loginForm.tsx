038cf2742c69b4a55c9b15ee0a552a7945385c5e
Harsh Rastogi <rastogiharsh04@gmail.com>
2023-05-06 23:15:53 +0530

    login validators added

--- ui/react/src/containers/login/loginForm.tsx
+++ ui/react/src/containers/login/loginForm.tsx
@@ -1,8 +1,7 @@
 import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
-import { Button, Form, Input } from 'antd';
-import { type Rule } from 'antd/es/form';
-import { useState, type FC } from 'react';
-import { passwordValidator } from '../../helpers/validators';
+import { Alert, Button, Form, Input } from 'antd';
+import { useEffect, useState, type FC } from 'react';
+import { passwordRuleObj, validateEmailRuleObj, validateMessages } from './helper';
 import './loginForm.scss';
 
 const { useForm, Item } = Form;
@@ -12,67 +11,54 @@ interface LoginFormSchema {
 	password: string;
 }
 
-/* eslint-disable no-template-curly-in-string */
-const validateMessages = {
-	required: '${label} is required!',
-	types: {
-		email: '${label} is not a valid email!',
-		number: '${label} is not a valid number!',
-	},
-};
-/* eslint-disable no-template-curly-in-string */
+const setDisplayIcon = (visible: boolean): JSX.Element =>
+	visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />;
 
 export const LoginForm: FC = () => {
 	const [form] = useForm<LoginFormSchema>();
 	const [passwordVisible, setPasswordVisible] = useState<boolean>(false);
-	console.count('c');
+	const [formData, setFormData] = useState<LoginFormSchema | null>(null);
+	const [error] = useState<Error | null>(null);
 
-	const displayIcon = (visible: boolean): JSX.Element =>
-		visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />;
-
-	const validateEmailRuleObj: Rule = {
-		required: true,
-		type: 'email',
-	};
-
-	const passwordRuleObj: Rule = {
-		required: true,
-		validator: async (rule, value) => {
-			console.log(value, rule);
-			if ('field' in rule && typeof rule.field === 'string') {
-				const err = passwordValidator(value, rule.field);
-				if (err == null) return;
-				throw err;
-			}
-		},
-	};
+	useEffect(() => {
+		if (formData == null) {
+			return;
+		}
+		console.log(formData);
+	}, [formData]);
 
 	return (
-		<Form
-			className="login-form"
-			form={form}
-			layout="vertical"
-			validateMessages={validateMessages}
-			onFinish={(p) => {
-				console.log(p);
-			}}
-		>
-			<Item name="email" label="Email" required rules={[validateEmailRuleObj]}>
-				<Input placeholder="Enter your email" />
-			</Item>
+		<>
+			{error != null && (
+				<Alert className="login-alert" closable description={error.message} type="error" />
+			)}
+			<Form
+				className="login-form"
+				style={{ marginTop: error != null ? '1rem' : 0 }}
+				form={form}
+				layout="vertical"
+				validateMessages={validateMessages}
+				onFinish={(data) => {
+					setFormData(data);
+				}}
+			>
+				<Item name="email" label="Email" required rules={[validateEmailRuleObj]}>
+					<Input placeholder="Enter your email" />
+				</Item>
 
-			<Item name="password" label="Password" required rules={[passwordRuleObj]}>
-				<Input.Password
-					htmlSize={12}
-					placeholder="enter your password"
-					visibilityToggle={{ visible: passwordVisible, onVisibleChange: setPasswordVisible }}
-					iconRender={displayIcon}
-				/>
-			</Item>
+				<Item name="password" label="Password" required rules={[passwordRuleObj]}>
+					<Input.Password
+						htmlSize={12}
+						placeholder="enter your password"
+						visibilityToggle={{ visible: passwordVisible, onVisibleChange: setPasswordVisible }}
+						iconRender={setDisplayIcon}
+					/>
+				</Item>
 
-			<Button htmlType="submit" type="primary" property="submit">
-				Login
-			</Button>
-		</Form>
+				<Button htmlType="submit" type="primary" property="submit" size="large">
+					Login
+				</Button>
+			</Form>
+		</>
 	);
 };
--- ui/react/src/helpers/validators.ts
+++ ui/react/src/helpers/validators.ts
@@ -1,11 +1,14 @@
-export type ValidatorFn<T> = (value: T, name: string) => Error | null;
+export type ValidatorFn<T> = (value?: T, name?: string) => Error | null;
 
-export const passwordValidator: ValidatorFn<string> = (value, name: string) => {
+export const passwordValidator: ValidatorFn<string> = (value, name) => {
 	const MIN_LENGTH = 8;
 	const ALPHANUMERIC_LOWER_UPPER_SYMBOL_RGX =
 		/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,}$/;
 
-	if (value === '') return new Error(`${name} is required`);
+	// eslint-disable-next-line no-template-curly-in-string
+	name = name ?? '${label}';
+
+	if (value == null || value.length === 0) return new Error(`${name} is required!`);
 	if (value.length <= MIN_LENGTH) return new Error(`${name} min length should be ${MIN_LENGTH}`);
 	if (!ALPHANUMERIC_LOWER_UPPER_SYMBOL_RGX.test(value))
 		return new Error(`${name} should contain a lowercase, uppercase, digit, and symbol`);
