186f4677a7ce8ce295a4ec9f601a9f88852323fe
Harsh Rastogi <rastogiharsh04@gmail.com>
2023-05-17 20:41:31 +0530

    create prescription upload form

--- ui/react/src/containers/Prescription/Prescription.tsx
+++ ui/react/src/containers/Prescription/Prescription.tsx
@@ -1,4 +1,5 @@
-import { Skeleton } from 'antd';
+import { InboxOutlined, PlusCircleOutlined } from '@ant-design/icons';
+import { Button, Modal, Skeleton, Upload } from 'antd';
 import { useEffect, useState, type FC } from 'react';
 import { type IPrescription } from './IPrescription';
 import './Prescription.scss';
@@ -35,7 +36,17 @@ const prescriptionList = [
 
 export const Prescription: FC = () => {
 	const [prescriptions, setPrescriptions] = useState<IPrescription[]>([]);
+	const [isVisible, setIsVisible] = useState(false);
 	const [loading, setLoading] = useState(true);
+
+	const modal = {
+		open() {
+			setIsVisible(true);
+		},
+		close() {
+			setIsVisible(false);
+		},
+	};
 
 	useEffect(() => {
 		const timeoutId = setTimeout(() => {
@@ -52,6 +63,23 @@ export const Prescription: FC = () => {
 			<Skeleton active loading={loading} />
 			<Skeleton active loading={loading} />
 			<PrescriptionList items={prescriptions} />
+			<span className="modal-button">
+				<Button type="primary" icon={<PlusCircleOutlined />} onClick={modal.open} />
+				<Modal centered width={800} open={isVisible} onOk={modal.close} onCancel={modal.close}>
+					<div>
+						<Upload.Dragger name="prescription">
+							<p className="ant-upload-drag-icon">
+								<InboxOutlined />
+							</p>
+							<p className="ant-upload-text">Click or drag file to this area to upload</p>
+							<p className="ant-upload-hint">
+								Support for a single or bulk upload. Strictly prohibited from uploading company data
+								or other banned files.
+							</p>
+						</Upload.Dragger>
+					</div>
+				</Modal>
+			</span>
 		</section>
 	);
 };
