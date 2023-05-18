ac66d14923fa93b2eb9496a644ef2b63b97fffe2
Harsh Rastogi <rastogiharsh04@gmail.com>
2023-05-18 17:59:36 +0530

    create scrollable container

---
+++ ui/react/src/components/ScrollableContainer/ScrollableContainer.tsx
@@ -0,0 +1,27 @@
+import classNames from 'classnames';
+import { type DetailedHTMLProps, type FC, type ReactNode } from 'react';
+import './ScrollableContainer.scss';
+
+type ElementProps<T> = DetailedHTMLProps<React.HTMLAttributes<T>, T>;
+
+interface ScrollableContainerProps {
+	title?: string;
+
+	children: ReactNode;
+}
+
+export const ScrollableContainer: FC<ScrollableContainerProps & ElementProps<HTMLDivElement>> = (
+	props
+) => {
+	const { title, className, children, ...rest } = props;
+
+	return (
+		<div className={classNames('scrollable-container-wrapper', className)} {...rest}>
+			<header className="header">
+				<span className="title">{title}</span>
+			</header>
+
+			<div className="scrollable-container">{children}</div>
+		</div>
+	);
+};
