b69833bda72ab33303f79716ad69b2393afbe853
Manpreet Kaur <manpreet.lawane@gmail.com>
2023-06-09 12:49:47 +0530

    prescription details can be fetched now

--- cmd/pharma_connect_chatbot/scripts/logics.py
+++ cmd/pharma_connect_chatbot/scripts/logics.py
@@ -16,104 +16,121 @@ cur = conn.connection()
 
 
 class Logics(metaclass=Singleton):
-    @classmethod
-    def get_words(cls, paragraph: str) -> list:
-        """
-        Method for get_lemmatized_words
+	@classmethod
+	def get_words(cls, paragraph: str) -> list:
+		"""
+			Method for get_lemmatized_words
 
-        :param paragraph: str
-        :return: list
-        """
-        words_list = []
-        try:
-            # tokenize paragraph
-            tokens = word_tokenize(paragraph)
+			:param paragraph: str
+			:return: list
+		"""
+		words_list = []
+		try:
+			# tokenize paragraph
+			tokens = word_tokenize(paragraph)
 
-            # remove stopwords from tokenize paragraph
-            stop_words = set(stopwords.words("english"))
-            filtered_tokens = [
-                token for token in tokens if token.lower() not in stop_words
-            ]
+			# remove stopwords from tokenize paragraph
+			stop_words = set(stopwords.words("english"))
+			filtered_tokens = [
+					token for token in tokens if token.lower() not in stop_words
+			]
 
-            # Lamatize paragraph
-            lemmatizer = WordNetLemmatizer()
-            words_list = [lemmatizer.lemmatize(token) for token in filtered_tokens]
+			# Lamatize paragraph
+			lemmatizer = WordNetLemmatizer()
+			words_list = [lemmatizer.lemmatize(token) for token in filtered_tokens]
 
-        except Exception as error:
-            logger.exception(error)
-        finally:
-            return words_list
+		except Exception as error:
+			logger.exception(error)
+		finally:
+			return words_list
+	@classmethod
+	def get_fallback_response(cls, user_message: str) -> str:
+		"""
+		Method to get_fallback_response for Logics
 
-    @classmethod
-    def get_fallback_response(cls, user_message: str) -> str:
-        """
-        Method to get_fallback_response for Logics
+		:param user_message: str
+		:return: str
+		"""
 
-        :param user_message: str
-        :return: str
-        """
+		resp = ""
+		try:
+			logger.info("Method to get_fallback_response for Logics")
+			logger.info(f"user_message = {user_message}")
 
-        resp = ""
-        try:
-            logger.info("Method to get_fallback_response for Logics")
-            logger.info(f"user_message = {user_message}")
+			resp = user_message
+		except Exception as error:
+			logger.exception(error)
+		finally:
+			return resp
 
-            resp = user_message
-        except Exception as error:
-            logger.exception(error)
-        finally:
-            return resp
+	@classmethod
+	def get_symptoms(cls, words: list) -> list:
+		"""
+		Method to get_symptoms for Logics
 
-    @classmethod
-    def get_symptoms(cls, words: list) -> list:
-        """
-        Method to get_symptoms for Logics
+		:param words: list
+		:return: list
+		"""
 
-        :param words: list
-        :return: list
-        """
+		response = []
+		try:
+			logger.info("Method to get_symptoms for Logics")
+			query = f"SELECT SUBSTRING(symptoms, CHARINDEX('{words[0]}', symptoms), LEN({len(words[0])} + 50000)) FROM diseases_data WHERE symptoms LIKE '%{words[0]}%';"
+			symptoms = cur.execute(query)
 
-        response = []
-        try:
-            logger.info("Method to get_symptoms for Logics")
-            query = f"SELECT SUBSTRING(symptoms, CHARINDEX('{words[0]}', symptoms), LEN({len(words[0])} + 50000)) FROM diseases_data WHERE symptoms LIKE '%{words[0]}%';"
-            symptoms = cur.execute(query)
+			df_symptoms = pd.DataFrame(symptoms, columns=["symptom"])
+			logger.info(f"df_symptoms: {df_symptoms}")
+			response = list(set([symptom[0] for symptom in  df_symptoms["symptom"].fillna("").tolist()]))
 
-            df_symptoms = pd.DataFrame(symptoms, columns=["symptom"])
-            logger.info(f"df_symptoms: {df_symptoms}")
-            response = list(set([symptom[0] for symptom in  df_symptoms["symptom"].fillna("").tolist()]))
+		except Exception as error:
+			logger.exception(error)
 
-        except Exception as error:
-            logger.exception(error)
+		finally:
+			return response
 
-        finally:
-            return response
+	@classmethod
+	def get_disease_by_symptoms(cls, symptoms: list) -> list:
+		"""
+		Method to get_disease_by_symptoms
 
-    @classmethod
-    def get_disease_by_symptoms(cls, symptoms: list) -> list:
-        """
-        Method to get_symptoms for Logics
+		:param symptoms: list
+		:return: list
+		"""
 
-        :param symptoms: list
-        :return: list
-        """
+		response = []
+		try:
+			logger.info(f"symptoms[0]: {type(symptoms[0]), symptoms[0]}")
+			query = "SELECT disease_name FROM diseases_data WHERE symptoms "
+			query += " OR ".join([f"speciality LIKE '%{symptom}%'" for symptom in symptoms])
+			diseases = cur.execute(query)
 
-        response = []
-        try:
-            logger.info(f"symptoms[0]: {type(symptoms[0]), symptoms[0]}")
-            query = "SELECT disease_name FROM diseases_data WHERE symptoms "
-            for i in range(len(symptoms)):
-	              query += f" like '%{symptoms[i]}%'"
-	              if i != len(symptoms) -1:
-			              query += " OR "
-            diseases = cur.execute(query)
+			df_diseases = pd.DataFrame(diseases, columns=["disease_name"])
+			logger.info(f"df_diseases: {df_diseases}")
+			response = list(set(df_diseases["disease_name"].fillna("").tolist()))
 
-            df_diseases = pd.DataFrame(diseases, columns=["disease_name"])
-            logger.info(f"df_diseases: {df_diseases}")
-            response = df_diseases["disease_name"].fillna("").tolist()
+		except Exception as error:
+			logger.exception(error)
 
-        except Exception as error:
-            logger.exception(error)
+		finally:
+			return response
+	@classmethod
+	def get_doctors(cls, diseases: list) -> list:
+		"""
+		Method to get_doctors
+		:param words: list
+		:return: list
+		"""
+		response = []
+		try:
+			logger.info("Methos to get_doctors")
+			query = f"Select name FROM doctors WHERE speciality "
+			query += " OR ".join([f"speciality LIKE '%{disease}%'" for disease in diseases])
+			doctors = cur.execute(query)
 
-        finally:
-            return response
+			df_doctors = pd.DataFrame(doctors, columns=["doctor_name"])
+			logger.info(f"df_doctors: {df_doctors}")
+			response = list(set(df_doctors["doctor_name"].fillna("").tolist()))
+		except Exception as error:
+			logger.exception(error)
+		finally:
+			return response
