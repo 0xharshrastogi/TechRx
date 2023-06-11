7bcb5844a380ad194cc064e3d04ce5ff378eb3c3
Harsh Rastogi <rastogiharsh04@gmail.com>
2023-06-11 14:53:40 +0530

    refactored random function

---
+++ cmd/data-generator/generated/doctors.sql
@@ -0,0 +1,12 @@
+INSERT INTO [Doctors]
+VALUES
+	('26b06', 'Cortez Marvin', 'A, D', 11, 'D1, D3, D4'),
+	('5814f', 'Jena Jacobi', 'C', 10, 'D1, D2'),
+	('bbc27', 'Skyla Kemmer', 'A, D', 1, 'D1, D4'),
+	('28bc3', 'Joana Friesen', 'A, C', 10, 'D1, D4'),
+	('ae8fb', 'Margarete Connelly', 'C, A', 19, 'D3, D2, D1'),
+	('5dc9f', 'Clinton Kessler', 'A, B', 0, 'D3, D4'),
+	('971d9', 'Kurtis Kertzmann', 'C, A', 3, 'D4, D2, D3'),
+	('400f6', 'Ernesto Brown', 'D, A', 3, 'D3, D4, D2'),
+	('b5352', 'Devyn Ledner', 'C', 5, 'D2, D4'),
+	('48787', 'Rhiannon Schinner', 'A, D', 16, 'D1, D2')
--- cmd/data-generator/index.ts
+++ cmd/data-generator/index.ts
@@ -1,7 +1,8 @@
-import { faker, fi } from '@faker-js/faker';
+import { faker } from '@faker-js/faker';
 import { writeFile } from 'fs/promises';
 import { SqlTableBuilder } from './src/builders';
 import { SqlQueryGenerator } from './src/generators';
+import { loadArrayFromFile, random } from './src/utils';
 
 const config = {
 	tableName: 'Doctors',
@@ -18,19 +19,24 @@ const generator = new SqlQueryGenerator(
 	})
 );
 
-const result = generator.generate(config.count, {
-	id: () => faker.number.hex({ min: 100000, max: 999999 }),
-	name: () => `${faker.person.firstName()} ${faker.person.lastName()}`,
-	specialty: () => faker.date.month(),
-	diseases: () => 'Tuberculosis',
-	experience: () => 4,
-});
+const filepath = 'generated/' + `${config.tableName}.sql`;
 
-const filepath = 'generated/' + `${config.tableName}.sql`
-writeFile(filepath, result)
-	.then(() => {
-		console.log('file generated successfully:', filepath);
-	})
-	.catch((error) => {
-		console.error('failed to generate file', error);
+async function main() {
+	const [specialties, diseases] = await Promise.all([
+		loadArrayFromFile('specialization.json'),
+		loadArrayFromFile('diseases.json'),
+	]);
+
+	const result = generator.generate(config.count, {
+		id: () => faker.number.hex({ min: 100000, max: 999999 }),
+		name: () => `${faker.person.firstName()} ${faker.person.lastName()}`,
+		diseases: () => random({ items: diseases, max: 3 }).join(', '),
+		experience: () => random(0, 20),
+		specialty: () => random({ items: specialties, max: 2 }).join(', '),
 	});
+	random;
+	await writeFile(filepath, result);
+	console.log('file written successfully');
+}
+
+main();
